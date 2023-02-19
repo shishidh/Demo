1312b18aca672ad649ee5cdc9581babec71dfca8
Daniel <daniel20160722@gmail.com>
2023-02-20 02:47:31 +0800

    更新排序判斷

--- admin/ad_list_sort.php
+++ admin/ad_list_sort.php
@@ -46,32 +46,42 @@ if (empty($post['id']) || empty($post['new_sort']) || empty($post['old_sort'])) 
     exit(json_encode($return));
 }
 
-$this_class = $db->where('id', $post['id'])->get($type_text);
+$all_list = $db->orderBy('sort', 'asc')->get($type_text);
 
-if (empty($this_class)) {
+foreach ($all_list as $key => $value) {
+    if ($key + 1 != $value['sort']) {
+        $return['code'] = 0;
+        $return['msg'] = '排序之中少了' . ($key + 1) . '，為了不影響其他排序，請先補上';
+        exit(json_encode($return));
+    }
+}
+
+$this_list = $db->where('id', $post['id'])->get($type_text);
+
+if (empty($this_list)) {
     $return['code'] = 0;
     $return['msg'] = '廣告不存在';
     exit(json_encode($return));
 }
 
-$two_class = $post['new_sort'] == $post['old_sort'] - 1 || $post['new_sort'] - 1 == $post['old_sort'];
+$two_list = $post['new_sort'] == $post['old_sort'] - 1 || $post['new_sort'] - 1 == $post['old_sort'];
 
-if ($two_class) {
+if ($two_list) {
     if ($post['new_sort'] == $post['old_sort'] - 1) {
-        $between_class = $db->where('sort', array($post['old_sort'] - 1, $post['new_sort']), 'BETWEEN')->get($type_text);
+        $between_list = $db->where('sort', array($post['old_sort'] - 1, $post['new_sort']), 'BETWEEN')->get($type_text);
     } else {
-        $between_class = $db->where('sort', array($post['new_sort'], $post['old_sort'] + 1), 'BETWEEN')->get($type_text);
+        $between_list = $db->where('sort', array($post['new_sort'], $post['old_sort'] + 1), 'BETWEEN')->get($type_text);
     }
 } else {
     if ($post['new_sort'] > $post['old_sort']) {
-        $between_class = $db->where('sort', array($post['old_sort'] - 1, $post['new_sort']), 'BETWEEN')->get($type_text);
+        $between_list = $db->where('sort', array($post['old_sort'], $post['new_sort']), 'BETWEEN')->get($type_text);
     } else {
-        $between_class = $db->where('sort', array($post['new_sort'], $post['old_sort'] - 1), 'BETWEEN')->get($type_text);
+        $between_list = $db->where('sort', array($post['new_sort'], $post['old_sort']), 'BETWEEN')->get($type_text);
     }
 }
 
 $updateData = "";
-foreach ($between_class as &$value) {
+foreach ($between_list as &$value) {
     if ($value['id'] != $post['id']) {
         $this_id = $value['id'];
         $this_type_id = $value['type_id'];
